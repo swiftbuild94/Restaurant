27f616fa03f9b8baf3accf444407131a2ebfe80a
SwiftBuild94 <swiftbuild94@gmail.com>
2021-02-19 01:16:48 -0300

    Decodable

--- Restaurant/Model/Restaurant.swift
+++ Restaurant/Model/Restaurant.swift
@@ -7,17 +7,34 @@
 
 import Foundation
 
-struct Restaurant {
+struct Restaurant: Decodable {
 	
-	var images: Array<String>?
+	var id: Int?
 	var name: String? = "Katmandou Café"
-	var address: String? = "14, rue Bréguet, Paris"
-	var typeOfFood: String? = "Indian"
-	var price: String? = "Average price €27"
-	var forkScore: Float? = 9.2
-	var forkReviews: Int? = 5552
-	var tripAdvisorScore: Int? = 4
-	var tripAdvisorReviews: Int? = 205
-	var geolocation: (lat:String, long:String)?
-
+	var address: String? = "14, rue Bréguet"
+	var city: String? = "Paris"
+	var zipcode: Int?
+	var currency_code: String?
+	var speciality: String? = "Indian"
+	var card_price: Int? = 27
+	var avg_rate: Float? = 9.2
+	var rate_count: Int? = 5552
+	var tripadvisor_avg_rate: Int? = 4
+	var tripadvisor_rate_count: Int? = 205
+	var gps_lat: Float?
+	var gps_long: Float?
+	var url: String?
+	var pics_diaporama: [String]?
+	
+	mutating func decodeJson(data: String)-> Bool {
+		let jsonData = data.data(using: .utf8)!
+		let restaurant = try? JSONDecoder().decode(Restaurant.self, from: jsonData)
+		if let result = restaurant {
+			print(result)
+			return true
+		}else{
+			return false
+		}
+	}
+	
 }
