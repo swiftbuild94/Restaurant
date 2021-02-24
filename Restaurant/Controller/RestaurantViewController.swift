0195128d6eb86bf5b0f098ebebe96d3e94637a0b
SwiftBuild94 <swiftbuild94@gmail.com>
2021-02-23 23:32:15 -0300

    Changes to getJson

--- Restaurant/Controller/RestaurantViewController.swift
+++ Restaurant/Controller/RestaurantViewController.swift
@@ -12,9 +12,28 @@ class RestaurantViewController: UIViewController {
 	private var restaurant = Restaurant()
 	private let url = "https://ptitchevreuil.github.io/test.json"
 	
-	private let displayView() {
 	
+	
+	private func displayView() {
+//		displayImages()
+//		displayName()
+		/*
+		if let address = restaurant.address {
+			let fullAddress = address + ", " + restaurant.city!
+			showInfo(.address, text: fullAddress)
+		}
+		if let foodType = restaurant.speciality {
+			showInfo(.address, text: foodType)
+		}
+		if let price = restaurant.card_price {
+			let currency_code = restaurant.currency_code ?? "$"
+			let fullPrice = "Average price " + currency_code + String(price)
+			showInfo(.price, text: fullPrice)
+		}
+		displayMap()
+		*/
 	}
+	
 	
 	private let loadingSpinner: UIActivityIndicatorView = {
 		let loadingSpinner = UIActivityIndicatorView(style: .gray)
@@ -31,31 +50,29 @@ class RestaurantViewController: UIViewController {
 	
 	// MARK: - Get Json
 	private func getJson(){
-		DispatchQueue.global(qos: .userInitiated).async {
-			guard let url = URL(string: self.url) else { return }
-			guard let data = try? Data(contentsOf: url) else { return }
-			do {
-				let serializedData = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-				print(serializedData)
-				guard let json_data = serializedData as? String else { return }
-				print(json_data)
-				let gotData = self.restaurant.decodeJson(data: json_data)
-				if gotData {
-					DispatchQueue.main.async {
-						self.loadingSpinner.stopAnimating()
-						self.displayView()
-					}
-				}
-			} catch {
-				debugPrint(error)
+		print("getJson")
+		guard let url = URL(string: self.url) else { return }
+		let task = URLSession.shared.dataTask(with: url){
+			(data, response, error) in
+			guard let data = data else { return }
+			print("Data: \(data)")
+			let serializedData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+			guard let json_data = serializedData as? String else { return }
+			let gotData = self.restaurant.decodeJson(data: json_data)
+			print("Zip code: \(String(describing: self.restaurant.zipcode))")
+			if gotData {
+				self.loadingSpinner.stopAnimating()
+				self.displayView()s
 			}
 		}
+		task.resume()
 	}
 	
 	
 	// MARK: - View Lifecycle
 	override func viewDidLoad() {
-		showSpinner()
+		view.backgroundColor = .white
+		self.showSpinner()
 		getJson()
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
